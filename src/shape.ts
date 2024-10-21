ecad3a250142228cce976bb84cec7fb8c16d8018
kỳ.vũ <phucky1235@gmail.com>
2024-10-21 15:35:17 +0700

    Add files via upload

---
+++ src/shape.ts
@@ -0,0 +1,28 @@
+export class Shape {
+    private _x : number;
+    private _y : number;
+
+    constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
+    }
+
+    get y(){
+        return this._y;
+    }
+    set y(y: number) {
+        this._y = y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set x(x: number) {
+        this._x = x;
+    }
+
+    toString(): String{
+        return `(x: ${this._x}, y: ${this._y})`;
+    }
+}
